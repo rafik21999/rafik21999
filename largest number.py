5684b647373014b882553769da5b9d8f72536382
rafik21999 <42027837+rafik21999@users.noreply.github.com>
2019-01-24 11:09:43 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if (a>b):
+ if (a>c):
+      print(a)
+elif (b>c):
+      print(b)
+else:
+      print(c)
